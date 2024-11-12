25de24d894bf0d559cda0b8ab0ccadece9d04777
Aniol Rodriguez <arodrigue96@gmail.com>
2024-11-12 20:02:41 +0100

    Move App component into components and add styles

---
+++ src/components/App/App.tsx
@@ -0,0 +1,13 @@
+import Header from "../Header/Header";
+import "./App.css";
+
+const App: React.FC = () => {
+  return (
+    <>
+      <Header />
+      <div className="container"></div>
+    </>
+  );
+};
+
+export default App;
