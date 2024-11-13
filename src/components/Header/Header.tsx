7e304d1b261ce645da9955dc69995b6368697efa
Aniol Rodriguez <arodrigue96@gmail.com>
2024-11-13 13:02:54 +0100

    Create NavMenu component and adapt it into Header component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,17 +1,21 @@
+import NavMenu from "../NavMenu/NavMenu";
 import "./Header.css";
 
 const Header: React.FC = () => {
   return (
     <header className="main-header">
       <div className="header-container">
-        <img
-          className="logo"
-          src="/logo.svg"
-          alt="Blog logo"
-          width={80}
-          height={80}
-        />
-        <h1 className="main-title">Aniol's Backoffice Blog</h1>
+        <div className="header-container__title">
+          <img
+            className="logo"
+            src="/logo.svg"
+            alt="Blog logo"
+            width={80}
+            height={80}
+          />
+          <h1 className="main-title">Aniol's Backoffice Blog</h1>
+        </div>
+        <NavMenu />
       </div>
     </header>
   );
