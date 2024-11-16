9b4476258e5c548ba8de46b765237d079dd376ed
Aniol Rodriguez <arodrigue96@gmail.com>
2024-11-17 00:24:34 +0100

    Add test for PostCard component

--- vite.config.ts
+++ vite.config.ts
@@ -9,5 +9,8 @@ export default defineConfig({
     globals: true,
     environment: "jsdom",
     setupFiles: ["src/setupTest.ts"],
+    coverage: {
+      include: ["src/**/*.tsx", "!src/main.tsx", "!src/router"],
+    },
   },
 });
