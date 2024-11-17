1db2073a20ab1a105c108d434f8d06838e78591f
Aniol Rodriguez <arodrigue96@gmail.com>
2024-11-17 22:55:59 +0100

    Correct image size to fix size and fix alternative text

--- src/pages/NotFoundPage/NotFoundPage.tsx
+++ src/pages/NotFoundPage/NotFoundPage.tsx
@@ -1,18 +1,17 @@
 import "./NotFoundPage.css";
 
 const NotFoundPage: React.FC = () => {
-  const image = "/carbassot.webp";
-  const imageAlt = "Carbassot image";
-  const imageWidthSize = 320;
-
   return (
     <div className="page-not-found-container">
       <h2 className="page-not-found__title">🙅🏼‍♂️ Page not found! 🙅🏼‍♂️</h2>
       <img
         className="page-not-found__image"
-        src={image}
-        alt={imageAlt}
-        width={imageWidthSize}
+        src="/carbassot.webp"
+        alt={
+          "Illustration of a page not found error with a pumpkin-shaped character"
+        }
+        width={300}
+        height={301}
       />
     </div>
   );
