20bae8daf909c2cabdde7ba1723f4965dbefdbb5
Aniol Rodriguez <arodrigue96@gmail.com>
2024-11-18 00:23:25 +0100

    Add PostCard image

--- src/post/components/PostCard/PostCard.tsx
+++ src/post/components/PostCard/PostCard.tsx
@@ -1,7 +1,10 @@
 import { Post } from "../../client/types";
+import "./PostCard.css";
 
 interface PostCardProps {
   post: Post;
+  imageUrl: string;
+  altImageText: string;
 }
 
 const PostCard: React.FC<PostCardProps> = ({ post }) => {
@@ -9,6 +12,13 @@ const PostCard: React.FC<PostCardProps> = ({ post }) => {
     <li>
       <article className="post-card">
         <h2 className="post-card__title">{post.title}</h2>
+        <img
+          className="post-card__image"
+          src={post.imageUrl}
+          alt={post.altImageText}
+          width={298}
+          height={199}
+        />
       </article>
     </li>
   );
