e09e9a020407388e6c12641473f587dd0c818c41
Aniol Rodriguez <arodrigue96@gmail.com>
2024-11-18 22:31:16 +0100

    Fix image quality and font-size

--- src/pages/NotFoundPage/NotFoundPage.tsx
+++ src/pages/NotFoundPage/NotFoundPage.tsx
@@ -2,14 +2,12 @@ import "./NotFoundPage.css";
 
 const NotFoundPage: React.FC = () => {
   return (
-    <div className="page-not-found-container">
+    <div className="page-not-found">
       <h2 className="page-not-found__title">🙅🏼‍♂️ Page not found! 🙅🏼‍♂️</h2>
       <img
         className="page-not-found__image"
         src="/images/carbassot.webp"
-        alt={
-          "Illustration of a page not found error with a pumpkin-shaped character"
-        }
+        alt="Illustration of a page not found error with a pumpkin-shaped character"
         width={300}
         height={301}
       />
