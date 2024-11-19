88b1bc346cf48914190fbec4ef9b3c69645a6ff7
Aniol Rodriguez <arodrigue96@gmail.com>
2024-11-19 22:57:39 +0100

    Add new test cases for PostCard and PostCardsLists components

--- src/post/components/PostCard/PostCard.test.tsx
+++ src/post/components/PostCard/PostCard.test.tsx
@@ -4,26 +4,35 @@ import { Post } from "../../client/types";
 
 describe("Given the PostCard component", () => {
   describe("When it receives a post with the title 'Exploring the Cryptocurrencies'", () => {
+    const bitcoin: Post = {
+      id: 1,
+      title: "Exploring the Cryptocurrencies",
+      imageUrl: "",
+      altImageText: "Bitcoin in all time high",
+      content: "",
+      author: "",
+      date: new Date(),
+    };
     test("Then it should show the post's title 'Exploring the Cryptocurrencies' inside a heading", async () => {
-      const post: Post = {
-        id: 1,
-        title: "Exploring the Cryptocurrencies",
-        imageUrl: "",
-        altImageText: "",
-        content: "",
-        author: "",
-        date: new Date(),
-      };
-
       const expectedTitle = /Exploring the Cryptocurrencies/i;
 
-      render(<PostCard post={post} />);
+      render(<PostCard post={bitcoin} />);
 
-      const postCardTitle = await screen.findByRole("heading", {
+      const postCardTitle = await screen.getByRole("heading", {
         name: expectedTitle,
       });
 
       expect(postCardTitle).toBeInTheDocument();
+    });
+
+    test("Then it should show an image with 'Bitcoin in all time high' alternative text", async () => {
+      const expectedAltImage = /Bitcoin in all time high/i;
+
+      render(<PostCard post={bitcoin} />);
+
+      const postCardAltImage = await screen.getByAltText(expectedAltImage);
+
+      expect(postCardAltImage).toBeInTheDocument();
     });
   });
 });
